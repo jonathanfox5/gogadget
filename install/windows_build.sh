95a84ee4da3e6d4d94948d41f4d1b2d27dd3de01
Jonathan Fox <jonathan_fox@hotmail.co.uk>
2024-11-19 20:01:41 +0000

    Documentation updates

---
+++ install/windows_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Clear out any wheels and existing installers that we have
+rm dist/*
+rm install/bin/*.whl
+
+# Build new wheel and copy to bin folder
+poetry build
+cp dist/*.whl install/bin/
+
+# Build windows installer and move to dist folder
+iscc install/gogadget_windows.iss
+mv install/output/*.exe dist/
+
+# Step just for people called "Jonathan Fox" ;) Updates pypi index.
+poetry publish
